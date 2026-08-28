044bae3f13cc24e893648fbc985c5798167e8141
Abhyam-mathur2 <abhyam.121822@stu.upes.ac.in>
2026-08-29 02:00:59 +0530

    Inverted-star-pyramid

---
+++ InvertedStarPyramid.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class InvertedStarPyramid {
+    public static void main(String[] args){
+        Scanner ob=new Scanner(System.in);
+        System.out.println("Enter a no");
+        int n=ob.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<2*(n-i)-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
