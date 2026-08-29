5e94425ea1a6ae1c2513deaf7e54bea4380e23fb
Abhyam-mathur2 <abhyam.121822@stu.upes.ac.in>
2026-08-29 16:46:08 +0530

    half diamond pattern

---
+++ halfdiamond.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class halfdiamond {
+    public static void main(String[] args){
+        Scanner ob=new Scanner(System.in);
+        System.out.println("Enter a no:");
+        int n=ob.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>0;i--){
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
