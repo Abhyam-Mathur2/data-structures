55267188925971a231cfcc59f9d9c0af3585ff15
Abhyam-mathur2 <abhyam.121822@stu.upes.ac.in>
2026-08-30 23:28:57 +0530

    Symmetric Void Pattern

--- SymmetricVoidPattern.java
+++ SymmetricVoidPattern.java
@@ -16,7 +16,7 @@ public class SymmetricVoidPattern {
             }
             System.out.println();
         }
-        for (int i = 1; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print("*");
             }
