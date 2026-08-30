2b8b04e810f4bcac6aba86c761a374038ad026ab
Abhyam-mathur2 <abhyam.121822@stu.upes.ac.in>
2026-08-31 03:42:56 +0530

    hollow rectangle pattern

---
+++ hollowrectpattern.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class hollowrectpattern {
+    public static void main(String[] args){
+        Scanner ob=new Scanner(System.in);
+        System.out.println("Enter a no: ");
+        int n=ob.nextInt();
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-1;j++){
+                if(i==0 || i==n-2 || j==0 || j==n-2){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
