78c56eed21da699172e2734ae874a126dea6fc22
Abhyam-mathur2 <abhyam.121822@stu.upes.ac.in>
2026-09-06 15:48:28 +0530

    reverse a no

---
+++ rverseano.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class rverseano {
+    public static void main(String[] args){
+        Scanner ob=new Scanner(System.in);
+        System.out.println("Enter a no:");
+        int n=ob.nextInt();
+        reversen(n);
+    }
+    public static void reversen(int n){
+        int ne=0;
+        while(n>0){
+            int d=n%10;
+            ne=ne*10+d;
+            n=n/10;
+        }
+        System.out.println(ne);
+    }
+}
