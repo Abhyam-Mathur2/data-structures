0fd23ff34cb848d376c7af003a56d687313a546e
Abhyam-mathur2 <abhyam.121822@stu.upes.ac.in>
2026-09-06 16:19:51 +0530

    Palindrome no

---
+++ palindrome.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class palindrome {
+    public static void main(String[] args){
+        Scanner ob=new Scanner(System.in);
+        System.out.println("Enter a no:");
+        int n=ob.nextInt();
+        pali(n);
+    }
+    public static void pali(int n){
+        int temp=n;
+        int c=0;
+        while(n>0){
+            int d=n%10;
+            c=c*10+d;
+            n=n/10;
+        }
+        if(c==temp){
+            System.out.println("Palindrome no");
+        }
+        else{
+            System.out.println("Not palindrome no");
+        }
+    }
+}
