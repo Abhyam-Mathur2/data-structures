3d8cf1378903c49d00e7d9ca18e8b36121ae7e49
Abhyam-mathur2 <abhyam.121822@stu.upes.ac.in>
2026-09-06 15:42:55 +0530

    count no of digit

---
+++ countdigit.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class countdigit {
+    public static void main(String[] args) {
+        Scanner ob = new Scanner(System.in);
+        System.out.println("Enter a no:");
+        int a=ob.nextInt();
+        int count=countd(a);
+        System.out.println(count);
+    }
+    public static int countd(int a ){
+        int c=0;
+        while(a>0){
+            c++;
+            a=a/10;
+            /*oprimal solution
+            int c=(int)(Math.log10(a)+1);
+             */
+        }
+        return c;
+    }
+}
