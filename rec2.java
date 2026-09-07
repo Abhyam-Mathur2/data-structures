66631d2a685c48cf9a5ca4d67fd95efe7035afbb
Abhyam-mathur2 <abhyam.121822@stu.upes.ac.in>
2026-09-08 02:50:43 +0530

    print no till n

---
+++ rec2.java
@@ -0,0 +1,16 @@
+import  java.util.Scanner;
+public class rec2 {
+    public static void main(String[] args){
+        Scanner ob=new Scanner(System.in);
+        System.out.println("Enter a no:");
+        int n=ob.nextInt();
+        rec(n);
+    }
+    public static void rec(int n){
+        if(n==0){
+            return;
+        }
+        System.out.print(n+" ");
+        rec(n-1);
+    }
+}
