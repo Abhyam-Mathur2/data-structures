d4d5e31258e6c194ad4af0c39357df3a5ac64063
Abhyam-mathur2 <abhyam.121822@stu.upes.ac.in>
2026-09-11 13:21:17 +0530

    largest element in java

---
+++ largestelement.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class largestelement {
+    public static void main(String[] args){
+        Scanner ob=new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int n=ob.nextInt();
+        int [] arr=new int[n];
+        System.out.println("Enter elements in array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i]=ob.nextInt();
+        }
+        // insertion sort
+        for (int i = 1; i <n ; i++) {
+            int key=arr[i];
+            int j=i-1;
+            while(j>=0 && arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+        System.out.println("largest element in array is: "+ arr[n-1]);
+    }
+}
